859e710f0ea9c8cdd78b984bb7eab160ff3105c7
David Valachovic <AdenFlorian@gmail.com>
2017-03-30 23:00:39 -0500

    created GatewayConnectionManager

---
+++ src/DiscordApiWrapper/Models/Gateway/GatewayResume.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace BundtBot.Discord.Models.Gateway {
+	[JsonObject]
+    public class GatewayResume {
+        [JsonRequired]
+        [JsonProperty("token")]
+        public string SessionToken;
+
+        [JsonRequired]
+        [JsonProperty("session_id")]
+        public string SessionId;
+
+        [JsonRequired]
+        [JsonProperty("seq")]
+        public int LastSequenceNumberReceived;
+	}
+}
