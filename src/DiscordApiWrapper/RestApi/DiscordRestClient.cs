bc5303efbaf09d2ae0ae35009070729034e177a2
David Valachovic <AdenFlorian@gmail.com>
2017-04-05 22:34:33 -0500

    cleaning

--- src/DiscordApiWrapper/RestApi/DiscordRestClient.cs
+++ src/DiscordApiWrapper/RestApi/DiscordRestClient.cs
@@ -2,10 +2,8 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading;
 using System.Threading.Tasks;
 using BundtBot.Extensions;
-using BundtCommon;
 using DiscordApiWrapper.RestApi;
 using DiscordApiWrapper.RestApi.RestApiRequests;
 
